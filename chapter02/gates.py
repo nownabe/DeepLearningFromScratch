339466079a88d89317c10a5b9be60ddc891e2822
nownabe <nownabe@gmail.com>
2017-02-09 20:31:04 +0900

    2.3.3

--- chapter02/gates.py
+++ chapter02/gates.py
@@ -1,12 +1,46 @@
-def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1 * w1 + x2 * w2
-    if tmp <= theta:
-        return 0
-    elif tmp > theta:
-        return 1
+import numpy as np
 
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    if np.sum(w*x) + b > 0:
+        return 1
+    else:
+        return 0
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    if np.sum(w*x) + b > 0:
+        return 1
+    else:
+        return 0
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    if np.sum(w*x) + b > 0:
+        return 1
+    else:
+        return 0
+
+print("and")
 print(AND(0, 0))
 print(AND(1, 0))
 print(AND(0, 1))
 print(AND(1, 1))
+
+print("nand")
+print(NAND(0, 0))
+print(NAND(1, 0))
+print(NAND(0, 1))
+print(NAND(1, 1))
+
+print("or")
+print(OR(0, 0))
+print(OR(1, 0))
+print(OR(0, 1))
+print(OR(1, 1))
